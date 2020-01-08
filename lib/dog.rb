b47b429a6f81fb1c6f7fcee0a608bc5e7b6521da
Mokhtar Ali <joemokhtarali@gmail.com>
2020-01-08 19:42:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,16 @@
 class Dog 
   
    def initialize(name) 
-    @name = name 
+    @this_dogs_name = name 
   end 
     
   def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
 
   
    def name
-    this_dogs_name
+    @this_dogs_name
   end
 end 
 
