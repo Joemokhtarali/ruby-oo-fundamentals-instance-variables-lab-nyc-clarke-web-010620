8b8d68c91849a86a0e721b42b3df7dd0e1d0aac2
Mokhtar Ali <joemokhtarali@gmail.com>
2020-01-08 19:37:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,20 @@
 class Dog 
   
- def name=(dog_name)
-   name = dog_name
- end 
-  
-  def name 
-    name 
+   def initalize(name) 
+    @name = name 
   end 
+    
+  def dog_name=(name) 
+   @name = name  
+  end 
+  
+# def name=(dog_name)
+#   name = dog_name
+# end 
+  
+#   def name 
+#     name 
+#   end 
 end 
 
 lassie = Dog.new("Lassie")
